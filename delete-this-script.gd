656aa9093212c7f5aeae0a4a7329e71bdb262bdd
Kayla Puig <119972216+KaylaPuig@users.noreply.github.com>
2023-03-25 13:58:45 -0400

    blablabla

--- delete-this-script.gd
+++ delete-this-script.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Sprite2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,5 +8,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
+	if Input.is_action_just_pressed("ui_left"):
+		position.x -= 0.1
+	if Input.is_action_just_pressed("ui_right"):
+		position.x += 0.1
