aa5083c0ee28f382d8c2094d13892e8df08032d7
Kayla Puig <119972216+KaylaPuig@users.noreply.github.com>
2023-03-25 19:49:03 -0400

    bleeehhhhhhhh

--- Dialogue Box.gd
+++ Dialogue Box.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+var current_string = []
+const FONTMAPCHARS = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+const BACKSPACECODE = 4194308
+const LINEWIDTH = 30
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,3 +13,31 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _input(event):
+	if event is InputEventKey and event.is_pressed():
+		if event.keycode == BACKSPACECODE:
+			remove_child(current_string.pop_back())
+		var c = str(char(event.unicode))
+		var ind = FONTMAPCHARS.find(c)
+		if ind >= 0 and ind < FONTMAPCHARS.length():
+			var child = load("res://Letter.tscn").instantiate()
+			child.show_character(c)
+			current_string.append(child)
+			update_lengths()
+			add_child(child)
+
+func show_string(string:String):
+	for c in current_string:
+		c.get_character()
+
+func delete_all_children():
+	for child in get_children():
+		remove_child(child);
+
+func update_lengths():
+	var i = 0
+	for c in current_string:
+		c.position.x = (i % LINEWIDTH) * 12
+		c.position.y = (i / LINEWIDTH) * 18
+		i += 1
--- Letter.gd
+++ Letter.gd
@@ -2,11 +2,15 @@ extends AnimatedSprite2D
 
 const FONTMAPCHARS = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
 
-func _input(event):
-	if event is InputEventKey:
-		var c = str(char(event.unicode))
-		show_character(c)
+var current_index = 0
+
+func _process(delta):
+	set_frame_and_progress(current_index, 0.0)
 
 func show_character(character:String):
 	var ind = FONTMAPCHARS.find(character.substr(0, 1))
-	set_frame_and_progress(ind, 0.0)
+	if ind >= 0 and ind <= FONTMAPCHARS.length():
+		current_index = ind
+
+func get_character() -> String:
+	return FONTMAPCHARS.substr(current_index, 1)
